64b49fe91e6c519b4bf572e0025cd1b73b79e132
Taibu Haroub <haroubtaibu@gmail.com>
2021-04-28 06:55:11 +0300

    Get the user location

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,17 +1,40 @@
 class PlaceFinder {
-    constructor() {
-        const addressForm = document.querySelector("form")
-        const locateUserBtn = document.getElementById("locate-btn")
+  constructor() {
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
 
-        locateUserBtn.addEventListener("click", this.locateUserHandler)
-        addressForm.addEventListener("click", this.findAddressHandler)
-    }
+    locateUserBtn.addEventListener("click", this.locateUserHandler);
+    addressForm.addEventListener("click", this.findAddressHandler);
+  }
 
-    locateUserHandler() {
-        //
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        "Location feature is not available in your browser - please use a more modern browser or manually address an address"
+      );
+      return;
     }
+    //getCurrentPostion takes 2 callback arguments
+    navigator.geolocation.getCurrentPosition(
+      (successResult) => {
+          console.log(successResult);
+          const coordinates = {
+              lat: successResult.coords.latitude,
+              lng: successResult.coords.longitude
+          }
+          console.log(coordinates);
+      },
+      (error) => {
+        alert(
+          "Could not locate you unfortunately. Please enter an address manually!"
+        );
+      }
+    );
+  }
 
-    findAddressHandler() {
-        //
-    }
+  findAddressHandler() {
+    //
+  }
 }
+
+new PlaceFinder();
